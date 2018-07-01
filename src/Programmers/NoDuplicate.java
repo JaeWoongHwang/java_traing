27f118684cc0fb6b4dabfd27317c86d77a3234af
JaeWoongHwang <jwngh15@gmail.com>
2018-07-01 23:54:33 +0900

    Today's training

--- src/Programmers/NoDuplicate.java
+++ src/Programmers/NoDuplicate.java
@@ -3,24 +3,27 @@ package Programmers;
 public class NoDuplicate {
 
 	public static void main(String[] args) {
-		int[] arr = { 1, 1, 3, 3, 0, 1, 1 };
+		int[] arr = { 4, 4, 4, 4, 3, 3 };
 		int[] arr2 = new int[arr.length];
 
 		for (int i = 0; i < arr.length; i++) {
-			while (i != arr.length) {
-				if (arr[i] == arr[i + 1]) {
+			if (i + 1 < arr.length) {
+				if (arr[i] != arr[i + 1]) {
 					arr2[i] = arr[i];
 				} else {
-					break;
+					
+					continue;
 				}
+			} else {
+				if (i == arr.length) {
+					arr2[i] = arr[i];
+				}
+				break;
 			}
 
 		}
-
 		for (int i = 0; i < arr2.length; i++) {
 			System.out.println(arr2[i]);
 		}
-
 	}
-
 }
