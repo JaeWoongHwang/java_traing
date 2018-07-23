3d71a31c4d9e6b8d41d02672d3a34a1eb6c4204a
JaeWoongHwang <jwngh15@gmail.com>
2018-07-23 18:52:59 +0900

    Today's training

--- src/Programmers/WordTranslation.java
+++ src/Programmers/WordTranslation.java
@@ -5,19 +5,27 @@ public class WordTranslation {
 	public static void main(String[] args) {
 		String answer = "try hello world";
 		String[] word = answer.split("\\s");
-		char [] array;
+		char[] array = new char[word.length];
+		answer = "";
+		
 		for (int i = 0; i < word.length; i++) {
 			// 스트링을 캐릭터로 변환
 			array[i] = word[i].charAt(i);
 
 			if (i % 2 == 0) {
-				array[i].toUpperCase();
+				word[i] = String.valueOf(array[i]);
+				word[i].toUpperCase();
+
 			} else {
-				array[i].toLowerCase();
+				word[i] = String.valueOf(array[i]);
+				word[i].toLowerCase();
+
 			}
+			
+			answer += word[i];
 		}
 
-		// System.out.println(answer);
+		System.out.println(answer);
 
 	}
 
