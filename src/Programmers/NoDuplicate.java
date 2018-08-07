22a65a27055edadddef5409ee7a12ca45d004ef8
JaeWoongHwang <jwngh15@gmail.com>
2018-08-07 17:29:41 +0900

    Today's training

--- src/Programmers/NoDuplicate.java
+++ src/Programmers/NoDuplicate.java
@@ -1,20 +1,27 @@
 package Programmers;
 
+import java.util.*;
+
 public class NoDuplicate {
 
 	public static void main(String[] args) {
-		int[] array = { 4, 4, 4, 4, 3, 3 };
-
-		for (int i = 0; i < array.length; i++) {
-			if (array[i] != array.length) {
-				if (array[i] == array[i + 1]) {
-					array.splice(i, 1);
+		int[] arr = { 4, 4, 4, 3, 3 };
+		List<Integer> result = new ArrayList<>();
+		
+		for (int i = 0; i < arr.length - 1; i++) {
+			if (arr[i] != arr[i + 1]) {
+				result.add(arr[i]);
+				continue;
+			} else {
+				if (i + 1 == arr.length - 1) {
+					result.add(arr[i]);
 				}
+				continue;
 			}
-			break;
 		}
-		for (int i = 0; i < array.length; i++) {
-			System.out.println(array[i]);
+		int[] answer = new int[result.size()];
+		for (int i = 0; i < result.size(); i++) {
+			answer[i] = result.get(i);
 		}
 	}
 }
